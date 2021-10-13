aa744e3e1459ac96ed77b65c0431a7d5b33bf936
mainuser <mainuser@DESKTOP-VRTAIJ0>
2021-10-13 15:34:35 -0400

    updated with a transaction scope

--- ODataBatching8/Extensions/CustomODataBatchHandler.cs
+++ ODataBatching8/Extensions/CustomODataBatchHandler.cs
@@ -10,6 +10,7 @@ using System.Configuration;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Net;
+using System.Transactions;
 
 namespace ODataBatching8
 {
@@ -21,82 +22,14 @@ namespace ODataBatching8
             _config = configuratiuon;
         }
 
-        
-
-        public async override Task<IList<ODataBatchResponseItem>> ExecuteRequestMessagesAsync(
-           IEnumerable<ODataBatchRequestItem> requests,
-           RequestDelegate handler)
+        public override async Task ProcessBatchAsync(HttpContext context, RequestDelegate nextHandler)
         {
-            if (requests == null)
+            using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
             {
-                throw new ArgumentNullException("requests");
+                await base.ProcessBatchAsync(context, nextHandler);
             }
-
-            IList<ODataBatchResponseItem> responses = new List<ODataBatchResponseItem>();
-            try
-            {
-                foreach (ODataBatchRequestItem request in requests)
-                {
-                    var operation = request as OperationRequestItem;
-                    if (operation != null)
-                    {
-                        responses.Add(await request.SendRequestAsync(handler));
-                    }
-                    else
-                    {
-                        await ExecuteChangeSet((ChangeSetRequestItem)request, handler, responses);
-                    }
-                }
-            }
-            catch
-            {
-                foreach (ODataBatchResponseItem response in responses)
-                {
-                    if (response != null)
-                    {
-                        throw;
-                    }
-                }
-                throw;
-            }
-
-            return responses;
         }
 
-        private async Task ExecuteChangeSet(ChangeSetRequestItem changeSet, RequestDelegate handler, IList<ODataBatchResponseItem> responses)
-        {
 
-            
-
-            using var context = DbContextFactory.Create(DBContextType.SqlServer, _config.GetConnectionString("BookDatabase"));
-
-            //Add the dbcontext into all of the changeset context
-            foreach (var request in changeSet.Contexts)
-            {
-                request.Items.TryAdd("Books_DBContext", context);
-            }
-
-            using var transaction = context.Database.BeginTransaction();
-
-            var changeSetResponse = (ChangeSetResponseItem)await changeSet.SendRequestAsync(handler);
-
-            if (changeSetResponse.Contexts.Any(r => r.Response.StatusCode != 200))
-            {
-                throw new Exception("Transaction failed"); 
-            }
-            responses.Add(changeSetResponse);
-
-            if (changeSetResponse.Contexts.All(r => r.Response.StatusCode == 200))
-            {
-                transaction.Commit();
-            }
-            else
-            {
-                transaction.Rollback();
-            }
-
-
-
-        }
     }
 }
