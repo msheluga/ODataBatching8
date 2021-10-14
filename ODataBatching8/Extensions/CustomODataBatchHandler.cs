39da931d55ea42e72632f45c4bb6f03407a7a016
mainuser <mainuser@DESKTOP-VRTAIJ0>
2021-10-14 13:08:20 -0400

    had to readd the factory as an easy way to generate the dbcontext in the batch handler

--- ODataBatching8/Extensions/CustomODataBatchHandler.cs
+++ ODataBatching8/Extensions/CustomODataBatchHandler.cs
@@ -4,12 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using System.Transactions;
 using Microsoft.OData;
 using Microsoft.AspNetCore.OData.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-
+using System.Linq;
+using ODataBatching8.Factories;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace ODataBatching8
 {
@@ -33,21 +34,32 @@ namespace ODataBatching8
                 throw new Exception(); //Error.ArgumentNull(nameof(handler));
             }
 
-            IList<ODataBatchResponseItem> responses = new List<ODataBatchResponseItem>();
+            
 
+            IList<ODataBatchResponseItem> responses = new List<ODataBatchResponseItem>();
+                              
             foreach (ODataBatchRequestItem request in requests)
             {
-                using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                using (var transaction = BookContextFactory.Create(DbContextType.SqlServer, _config.GetConnectionString("BookDatabase")).Database.BeginTransaction())
                 {
                     ODataBatchResponseItem responseItem = await request.SendRequestAsync(handler).ConfigureAwait(false);
                     responses.Add(responseItem);
 
-                    if (responseItem != null)// && responseItem.IsResponseSuccessful() == false && ContinueOnError == false)
+                    var wasSuccessful = responses.OfType<ChangeSetResponseItem>()
+                        .Select(r => r.Contexts.All(c => c.Response.IsSuccessStatusCode()))
+                        .All(c => c);
+
+                    if (responseItem != null && wasSuccessful)// && responseItem.IsResponseSuccessful() == false && ContinueOnError == false)
                     {
-                        break;
+                        transaction.Commit();
+                    }
+                    else
+                    {
+                        transaction.Rollback();
                     }
                 }
             }
+            
             return responses;
         }
 
